6e2a730f32d185e73c18be8d1d27ee450ff5a4e9
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-01-25 00:52:22 +0100

    Logging added elements to the console

--- app.js
+++ app.js
@@ -5,3 +5,7 @@ const linkList = LinkedList.arrayValues(20, 30, 40);
 linkList.printEelements(); 
 
 console.log(linkList.getElementByIndex(0).value);
+
+linkList.addElementByIndex(2, 45);
+
+linkList.printEelements(); 
