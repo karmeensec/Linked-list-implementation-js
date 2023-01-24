51ff4afde286bf2ff0800d07116fcc930cc14572
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-01-24 23:31:50 +0100

    Test cases for the index

--- linkedList.test.js
+++ linkedList.test.js
@@ -17,3 +17,63 @@ describe('#addToHead', ()=> { // testing specific method
     })
 
 })
+
+
+describe('#getElementByIndex', () => { 
+
+
+    describe('If we put index less than 0', ()=> {
+
+        test('Should return null', ()=> {
+
+            const linkList = LinkedList.arrayValues(5, 20);
+
+            expect(linkList.getElementByIndex(-1)).toBeNull();
+            
+
+        })
+
+    })
+
+
+    describe('If we put index greater than length of the list', ()=> {
+
+        test('Should return null', ()=> {
+
+            const linkList = LinkedList.arrayValues(5, 20);
+
+            expect(linkList.getElementByIndex(4)).toBeNull();
+
+        });
+
+    })
+
+
+    describe('if we put 0 as an index', ()=> {
+
+        test('Should return head', ()=> {  // first element should be head
+
+            const linkList = LinkedList.arrayValues(5, 20);
+
+            expect(linkList.getElementByIndex(0)).toBe(linkList.head);
+
+        });
+
+    });
+
+
+    describe('if we get index in the middle of the list', ()=> {
+
+        test ('Should return that element', ()=> {
+
+            const linkList = LinkedList.arrayValues(5, 15, 20, 25 );
+
+            expect(linkList.getElementByIndex(3).value).toBe(25);
+
+        });
+
+    });
+
+
+ })
+
