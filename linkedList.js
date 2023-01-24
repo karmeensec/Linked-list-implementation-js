7ef3e8d3445dfe1ac68c561bab70f6c9e0df3c90
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-01-24 20:17:29 +0100

    Creating classes with an initial values

---
+++ linkedList.js
@@ -0,0 +1,21 @@
+ export class linkedList {
+
+    constructor() {
+
+        this.head = null;
+        this.length = 0;
+
+    }
+
+}
+
+export class LinkedListNode {
+
+    constructor(value, next) {
+
+        this.value = value;
+        this.next = next;
+
+    }
+
+}
