0858a81d4a279717d49d057517f89d490b7a5679
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-01-25 14:20:56 +0100

    Manual testing in console

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const LinkedList = require('./linkedList');
 
+// .1
+
 const linkList = LinkedList.arrayValues(20, 30, 40);
 
 linkList.printEelements(); 
 
+// .2
+
 console.log(linkList.getElementByIndex(0).value);
+
+// .3
 
 linkList.addElementByIndex(2, 45);
 
 linkList.printEelements(); 
+
+// .4
+
+linkList.removeFromHead();
+
+linkList.printEelements();
